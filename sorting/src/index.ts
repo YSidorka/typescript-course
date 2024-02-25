8cac96575a0a0a67d9ae5ba3a910d3791ddba0b0
YSidorko <yury.sidorko@gmail.com>
2024-02-25 10:33:24 +0100

    Lec 95

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,7 +1,18 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharCollection } from './CharCollection';
+import { LinkedList } from './LinkedList';
 
-let sorter = new Sorter(new NumbersCollection([100, 5, -5, 0, 1]));
+// let sorter = new Sorter(new NumbersCollection([100, 5, -5, 0, 1]));
+// let sorter = new Sorter(new CharCollection(';lkjhgfdsertyhgvb'));
+const list = new LinkedList();
+
+list.add(100);
+list.add(5);
+list.add(-5);
+list.add(0);
+list.add(1);
+
+let sorter = new Sorter(list);
 sorter.sort();
 console.log(sorter.collection);
